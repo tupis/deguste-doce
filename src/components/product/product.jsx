529814bf2323e3154a98a7233526db6179b09dba
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-14 11:14:54 -0300

    remoção de fragments desnecessários

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -9,26 +9,23 @@ import Information from './layout/information'
 //Image Test
 import Img1 from '../../images/img1.png'
 import Img2 from '../../images/img2.png'
-        // images sempre de 1:1
-
+    // images sempre de 1:1
 
 //Fake api
 const product = {
-    name: 'Bolo 5KG',
-    price: 150,
-    description: 'Contém pasta americana, voce pode escolher o recheio',
-    images: [Img1, Img2, Img1, Img2, Img1, Img2]
+    name: 'Bolo 10KG',
+    price: 300,
+    description: 'Contém pasta americana, e pasta de amendoim',
+    images: [Img1, Img2, Img1]
 }
 
 const Product = () => {
     return ( 
-        <>
-            <div className="product">
-                <CarouselImages images={product.images} />
-                <Information product={product} />
-                < AddToCart price={product.price}/>
-            </div>
-        </>
+        <div className="product">
+            <CarouselImages images={product.images} />
+            <Information product={product} />
+            < AddToCart price={product.price}/>
+        </div>
     );
 }
  
