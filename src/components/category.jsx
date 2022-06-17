11b5ff9b6701a2387dbd2482ab67795d2f8ba4d3
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-17 11:15:59 -0300

    automatizando a renderização da category e dos componentes filhos

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,14 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const Category = ( { category } ) => {
+
+    let categoryName = ''
+    
+    if(category.name){
+        categoryName = `/${(category.name).toLowerCase().replace(' ','')}`
+    }
+
     return (
-        <>
+        <>  
             <div className="category">
                 <div className="header-category">
                     <h1> {category.name} </h1>
-                    <p>Veja todos</p>
+                    < Link to={categoryName}>
+                        <p>Veja todos</p>
+                    </Link>
                 </div>
                 <div className='images-category'>
                     {
-                        Object.keys(category.products).map((product, value) => <img key={category.products} src='https://via.placeholder.com/50px' alt={`${product}`}/>)
+                        Object.keys(category.products).map((product) => <img key={category.products} src='https://via.placeholder.com/50px' alt={`${product}`}/>)
                     }
                 </div>
                 <hr />
