f455082a086091b8891343bc9bf566cd38865eb3
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-21 01:21:10 -0300

    troca de ancora normal, pra Link Router com endereçamento pra _id do produto

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,16 @@
-import Img from '../images/img1.png'
+import { Link } from 'react-router-dom'
 
-const Card = (  {product, key}  ) => {
+const Card = (  {product}  ) => {
     return (
-        <a className="wrapper-card" href="#" key={key}> 
+        < Link className="wrapper-card" to={`/product/${product._id}`}>
             <div className="card">
-                <img src={Img} alt="imagem" />
+                <img src={product.images[0]} alt="imagem" />
                 <div className="card-information">
                     <h1> {product.name} </h1>
                     <p>R$ {product.price},00</p>
                 </div>
             </div>
-        </a>
+        </Link>
     );
 }
  
