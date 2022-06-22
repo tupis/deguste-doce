e2cf2e504ee4370a8b40d11189fbe91d08c29b89
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-22 01:12:49 -0300

    Importação do Context no App

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import About from './components/about';
 import NavBar from "./components/navBar";
 import { useEffect, useState } from "react";
 import Products from './components/products';
+import CartProvider from './context/index';
+import Cart from "./components/cart";
 
 const App = () => {
   const [category, setCategory] = useState({})
@@ -27,23 +29,23 @@ const App = () => {
 
   return (
     <Router>
-      < NavBar />
-      < Routes >
-        < Route path="/" element={ < InitialPage category={category} /> } />
-        {/* < Route path="/shopping_cart" element={ < Cart /> } /> */}
-        < Route path="/about" element={ < About /> } />
-        < Route path='/product' element={ < Product /> } />
-        {
-          // eslint-disable-next-line
-          Object.values(category).map(category => < Route path={`/${(category.name).toLowerCase().replace(' ','')}`} element={ < Products category={category} /> } />) 
-        }
-        {
-          Object.values(category).map(category => category.products.map(product => < Route path={`/product/${product._id}`} element={ < Product product={product} back={`/${(category.name).toLowerCase().replace(' ','')}`} /> } />))
-        }
-        
-      </Routes>
+      < CartProvider >
+        < NavBar />
+        < Routes >
+          < Route path="/" element={ < InitialPage category={category} /> } />
+          < Route path="/shopping_cart" element={ < Cart /> } />
+          < Route path="/about" element={ < About /> } />
+          < Route path='/product' element={ < Product /> } />
+          {
+            // eslint-disable-next-line
+            Object.values(category).map(category => < Route path={`/${(category.name).toLowerCase().replace(' ','')}`} element={ < Products category={category} /> } />) 
+          }
+          {
+            Object.values(category).map(category => category.products.map(product => < Route path={`/product/${product._id}`} element={ < Product product={product} back={`/${(category.name).toLowerCase().replace(' ','')}`} /> } />))
+          }
+        </Routes>
+      </CartProvider>
     </Router>
-    
   );
 }
  
