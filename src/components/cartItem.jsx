b16f20add178056606295b966187c75b1f01d87f
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-23 21:57:04 -0300

    importação do useContext para utilizar a função removeItemCart

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { CartContext } from '../context'
 import API from '../services/axios'
 
 
 const CartItem = ({ product }) => {
     const [productAPI, setProductAPI] = useState({})
 
+    const { removeItemCart } = useContext(CartContext)
+
     useEffect(()=>{
         API.get(`/product/${product[0]}`).then(({data}) => {
             setProductAPI(data);
         });
         // eslint-disable-next-line
-    }, [])
+    }, [product])
 
     return (
         <div className="cartItem">
@@ -19,7 +22,7 @@ const CartItem = ({ product }) => {
                     <p className="quantity">{product[1]}x</p>
                     <h1 className="title-product">{productAPI.name}</h1>
                 </div>
-                <button className="btn-remove">Remover</button>
+                <button className="btn-remove" onClick={() => removeItemCart(product[0])}>Remover</button>
             </div>
             <p className="subtotal-product">R$ {product[2]},00 </p>
             <hr />
