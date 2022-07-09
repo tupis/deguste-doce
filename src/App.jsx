e38bf397ad9e5a23991bbb95225083c3950f2506
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-07-09 19:24:53 -0300

    adição de keys do map

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './sass/index.scss'
 
 //Components
 import InitialPage from "./components/pages/InitialPage";
-import Product from './components/product/product';
+import Product from './components/product/Product';
 import About from './components/About';
 import NavBar from "./components/pages/NavBar";
 import Products from './components/pages/Products';
@@ -20,14 +20,13 @@ import Cart from "./components/pages/Cart";
 
 
 const App = () => {
-  const [category, setCategory] = useState({})
+  const [category, setCategory] = useState([])
 
   useEffect(()=>{
     API.get('category').then(({data}) => {
       setCategory(data);
     })
 
-    // eslint-disable-next-line
   }, [])
 
   return (
@@ -40,12 +39,10 @@ const App = () => {
           < Route path="/about" element={ < About /> } />
           < Route path='/product' element={ < Product /> } />
           {
-            // eslint-disable-next-line
             Object.values(category).map((category, key) => < Route path={`/${(category.name).toLowerCase().replace(' ','')}`} element={ < Products category={category} /> } key={key} />) 
           }
           {
-            // eslint-disable-next-line
-            Object.values(category).map(category => category.products.map(product => < Route path={`/product/${product._id}`} element={ < Product product={product} back={`/${(category.name).toLowerCase().replace(' ','')}`} /> } />))
+            Object.values(category).map((category, key) => category.products.map(product => < Route path={`/product/${product._id}`} element={ < Product product={product} back={`/${(category.name).toLowerCase().replace(' ','')}`} /> } key={key} />))
           }
         </Routes>
       </CartProvider>
