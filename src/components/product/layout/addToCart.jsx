2cc48c734de30d7cd336b338eabfbb1c4c1484c8
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-07-09 19:36:57 -0300

    criação da função handleOnLick que chamas as outras; criação de função que ativa animação do botao de carrinho

--- src/components/product/layout/addToCart.jsx
+++ src/components/product/layout/addToCart.jsx
@@ -21,11 +21,25 @@ const AddToCart = ( {product, addCart, obs} ) => {
             setCount(count - 1);
         }
     }
+
+    const animationAddCart = () => {;
+        const buttonAddCart = document.querySelector('.add-cart')
+        buttonAddCart.classList.add('animation');
+        setTimeout(()=>{
+            buttonAddCart.classList.remove('animation');
+        }, 2000)
+    }
+
+    const handleOnClick = () => {
+        addTotalPrice(product._id, totalPriceProduct);
+        addCart(product._id, count, totalPriceProduct);
+        addMessageToArray(product.name, count, obs , product._id);
+        animationAddCart();
+    }
+
     
     useEffect(() => {
-        
         setTotalPriceProduct(count * product.price);
-        // eslint-disable-next-line
     },[count])
 
     return (
@@ -35,10 +49,13 @@ const AddToCart = ( {product, addCart, obs} ) => {
                 <button> {count} </button>
                 <button onClick={counterIncrease}> + </button>
             </div>
-            <button className='add-cart' onClick={() => addCart(product._id, count, totalPriceProduct)} onMouseDown={() => addMessageToArray(product.name, count, obs , product._id)} onMouseUp={() => addTotalPrice(product._id, totalPriceProduct)}> 
+            
+            <button className='add-cart' onClick={handleOnClick}> 
                 <div className='text-area'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z" fill="currentColor" /><path d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z" fill="currentColor" /><path d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z" fill="currentColor" /></svg>
-                    Adicionar carrinho
+                    <span class="material-symbols-outlined" id="cart">
+                        shopping_cart
+                    </span>
+                    <p>Adicionar ao carrinho</p>
                 </div>
                 <p> R$ {totalPriceProduct},00 </p>
             </button>
