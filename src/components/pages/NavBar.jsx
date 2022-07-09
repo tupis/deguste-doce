0ec136ba53b5d612377efa3642c5c1979518b834
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-07-09 19:18:38 -0300

    adição de um title

--- src/components/pages/NavBar.jsx
+++ src/components/pages/NavBar.jsx
@@ -3,26 +3,31 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="nav-bar">
-            <Link to='/'>
-                <span className="material-symbols-outlined">
-                    restaurant_menu
-                </span>
-                Cardápio
-            </Link>
-            <Link to='/shopping_cart'>
-                <span className="material-symbols-outlined">
-                    shopping_cart_checkout
-                </span>
-                Carrinho
-            </Link>
-            <Link to='/about'>
-                <span className="material-symbols-outlined">
-                    more_horiz
-                </span>
-                Informações
-            </Link>
-        </nav>
+        <div className="wrapper-navBar">
+            <div className="title-shop">
+                <h1>Deguste Doce</h1>
+            </div>
+            <nav className="nav-bar">
+                <Link to='/'>
+                    <span className="material-symbols-outlined">
+                        restaurant_menu
+                    </span>
+                    Cardápio
+                </Link>
+                <Link to='/shopping_cart'>
+                    <span className="material-symbols-outlined">
+                        shopping_cart_checkout
+                    </span>
+                    Carrinho
+                </Link>
+                <Link to='/about'>
+                    <span className="material-symbols-outlined">
+                        more_horiz
+                    </span>
+                    Informações
+                </Link>
+            </nav>
+        </div>
     );
 }
  
