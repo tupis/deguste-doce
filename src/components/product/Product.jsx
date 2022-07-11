7bfcf626c130014fae390ee299284770c16bad10
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-07-11 15:55:02 -0300

    criação de função para pegar o valor selecionado(sabor) + .map para renderizar os valores(sabores)

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -10,7 +10,7 @@ import Information from '../pages/Information';
 import ButtonClose from '../ButtonClose';
 import { CartContext } from '../../context/index';
 
-const Product = ( {product, back} ) => {
+const Product = ( {product, back, options} ) => {
     const { addItemCart } = useContext(CartContext);
 
     const [inputObs, setInputObs] = useState('Sem Observação')
@@ -19,6 +19,23 @@ const Product = ( {product, back} ) => {
         setInputObs(e.target.value)
     }
 
+    const [option, setOption] = useState('')
+
+    const getSelectedOption = () => {
+        let radioBtns = document.querySelectorAll("input[name='option']");
+
+        const findSelected = () => {
+            let selected = document.querySelector("input[name='option']:checked")
+            setOption(selected.value)
+        }
+
+        radioBtns.forEach(radioBtn => {
+            radioBtn.addEventListener("change", findSelected);
+        })
+    }
+
+    getSelectedOption();
+
     return ( 
         <div className="wrapper-product">
             <div className="product">
@@ -26,8 +43,17 @@ const Product = ( {product, back} ) => {
                 <CarouselImages images={product.images} />
                 <div className="wrapper-info-addCart">
                     <Information product={product} setInputObs={inputValue}/>
-                    <AddToCart product={product} addCart={addItemCart} obs={inputObs}/>
+                    <AddToCart product={product} option={option} addCart={addItemCart} obs={inputObs}/>
                 </div>
+            </div>
+            <div className="wrapper-options">
+            {
+                options.map(option => 
+                <div className="option" onClick={getSelectedOption}>
+                    <input type="radio" name="option" value={option} id={option}/>
+                    <label for="option">{option}</label>
+                </div>)
+            }
             </div>
         </div>
     );
